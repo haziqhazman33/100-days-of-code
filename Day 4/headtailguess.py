86adb165cbd6453802aa3f76c6a5fbf804d94945
HaziqHazman <109997255+haziqhazman33@users.noreply.github.com>
2024-11-28 12:19:03 +0800

    Delete Day 4 directory

--- Day 4/headtailguess.py
+++
@@ -1,8 +0,0 @@
-import random
-
-random_int=random.randint(0,1)
-yourchoice=int(input("guess either heads,0 or tails,1.input either number 0 or 1 \n"))
-if yourchoice==random_int:
-    print("congratulations,correct guess")
-else:
-    print("wrong answer")
