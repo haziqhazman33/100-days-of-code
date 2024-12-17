11133ef0b0edc18856e64fc156875721946bf698
HaziqHazman <109997255+haziqhazman33@users.noreply.github.com>
2024-12-17 10:51:29 +0800

    Create main.py

---
+++ snake_game part 1/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle,Screen
+from snake import Snake
+import time
+screen=Screen()
+screen.title('Snake Game')
+
+screen.setup(width=600,height=600)
+screen.bgcolor('black')
+screen.title("snake_game v2")
+screen.tracer(0) #
+
+game_on=True
+snake=Snake()
+
+screen.listen()
+screen.onkey(snake.up,"w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left,"a")
+screen.onkey(snake.right,"d")
+
+while game_on:
+    screen.update() 
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
